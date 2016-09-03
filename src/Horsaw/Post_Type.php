c6e931f712fa03e7460e352578385779b5957c3d
Joro Stoyanov <stoyanov.gs@gmail.com>
2016-09-03 13:46:42 +0300

    Fix Taxonomy Post Types registration

--- src/Horsaw/Post_Type.php
+++ src/Horsaw/Post_Type.php
@@ -42,6 +42,21 @@ class Post_Type {
 	}
 
 	/**
+	 * Sets the Taxonomy Post Types.
+	 *
+	 * @access public
+	 *
+	 * @param array $post_types
+	 *
+	 * @return $this
+	 */
+	public function set_post_types( $post_types ) {
+		$this->post_types = $post_types;
+
+		return $this;
+	}
+
+	/**
 	 * Registers a new Custom Post Type.
 	 *
 	 * @static
